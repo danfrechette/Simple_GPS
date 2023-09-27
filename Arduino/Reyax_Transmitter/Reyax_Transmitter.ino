159b05b9f3aeab9a575579b8992c0a15d1852b9d
danfrechette <dfrechette@gmail.com>
2023-09-27 06:31:59 -0400

    update

--- Arduino/Reyax_Transmitter/Reyax_Transmitter.ino
+++ Arduino/Reyax_Transmitter/Reyax_Transmitter.ino
@@ -9,20 +9,21 @@ String FREQUENCY_BAND = "433000000";
 String NETWORK_ID = "7";               // Recommended 1~15
 String NODE_ADDRESS_NATIVE = "1";
 String NODE_ADDRESS_FOREIGN = "2";
+int Baudrate = 9600;
 
 int Cntr = 0;
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+  // pinMode(LED_BUILTIN, OUTPUT);
 
   Serial.begin(9600); delay(1000);
   Serial.println("Starting Reyax Transmitter");
 
   lora_serial.begin(9600); delay(1000);
-  lora_serial.println("AT+IPR=9600");delay(1000);
   lora_serial.println("AT+BAND=" + FREQUENCY_BAND); delay(1000);
   lora_serial.println("AT+NETWORKID=" + NETWORK_ID); delay(1000);
   lora_serial.println("AT+ADDRESS=" + NODE_ADDRESS_NATIVE); delay(1000);
+  lora_serial.println("AT+IPR=9600");delay(1000);
   lora_serial.println("AT+PARAMETER=10,7,1,7");  delay(1000);
   lora_serial.println("AT+CPIN=FABC0002EEDCAA90FABC0002EEDCAA90"); delay(1000);
 
@@ -31,24 +32,24 @@ void setup() {
 
 void loop() {
 
-  String Message = "Test Message[" + String(++Cntr) + "]\r\n";
+  String Message = "Test Message[" + String(++Cntr) + "]";
 
   AT_Cmd = "AT+SEND=" + NODE_ADDRESS_FOREIGN + ",";
   AT_Cmd += String(Message.length() - 2);
   AT_Cmd += "," + Message;
 
-  lora_serial.println(AT_Cmd);
+  lora_serial.print(AT_Cmd); delay(1000);
   Serial.print("Transmitter outgoing: "); Serial.println(AT_Cmd);
 
   delay(1000);
 
-  while (lora_serial.available()) {
+  //while (lora_serial.available()) {
     // IncomingString = lora_serial.readString();
-    IncomingString = lora_serial.readStringUntil('\n');
-    if(IncomingString.length() > 2){
-      Serial.print("Transmitter incoming: "); Serial.println(IncomingString);
-    }
-  }
+  //  IncomingString = lora_serial.readStringUntil('\n');
+  //  if(IncomingString.length() > 2){
+  //    Serial.print("Transmitter incoming: "); Serial.println(IncomingString);
+  //  }
+  //}
 
-  digitalWrite(LED_BUILTIN, HIGH); delay(1000); digitalWrite(LED_BUILTIN, LOW);
+  // digitalWrite(LED_BUILTIN, HIGH); delay(1000); digitalWrite(LED_BUILTIN, LOW);
 }
